9ee0b261369af1c3a93b3e7afeefc6a1585b0871
nayan5565 <nayan5565@gmail.com>
2017-07-25 22:01:48 +0600

    init

---
+++ app/src/main/java/com/nayan/progressbaractivity/MainActivity.java
@@ -0,0 +1,70 @@
+package com.nayan.progressbaractivity;
+
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private TextView textView;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        textView = (TextView) findViewById(R.id.textView);
+        change();
+        // Start long running operation in a background thread
+
+    }
+
+    public void setProgressBar() {
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            textView.setText(progressStatus + "/" + progressBar.getMax());
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void change() {
+        progressStatus++;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (progressStatus > 100) {
+                    progressStatus=0;
+                }
+
+                progressBar.setProgress(progressStatus);
+                textView.setText(progressStatus + "/" + progressBar.getMax());
+                change();
+
+
+            }
+        }, 100);
+
+    }
+}
